9e40a32e37fc736925f7a8b9e0d86796d9af937b
nathanctva <nathanctva@gmail.com>
2026-02-20 23:55:44 -0300

    Entrega da Atividade - AULA 13 - Finalizada

--- Exercícios Mundo 2 - Curso em Vídeo/Desafio-56.py
+++ Exercícios Mundo 2 - Curso em Vídeo/Desafio-56.py
@@ -1,7 +1,11 @@
 # ATIVIDADE EM PROCESSO DE DESENVOLVIMENTO
 
+import sys
+
 pessoas = {}
 id = 1
+
+print('\033[33mPrograma de Lista de Pessoas\033[m\n')
 
 # Repetição de entrada para até 4 Pessoas:
 for pessoa in range(1, 5):
@@ -42,6 +46,8 @@ for pessoa in range(1, 5):
 
     id += 1
 
+print('---' * 20)
+
 # Listagem das Pessoas:
 for id, pessoa in pessoas.items():
     print(f'Identificador: {id}, Nome: {pessoa['nome']}, Idade: {pessoa['idade']}, Sexo: {pessoa['sexo']}')
@@ -50,14 +56,39 @@ print('')
 
 # Média de Idade do Grupo:
 media_idade = ((pessoas[1]['idade']) + (pessoas[2]['idade']) + (pessoas[3]['idade']) + (pessoas[4]['idade'])) / 4
-print(f'Média das Idades = {media_idade:.2f}')
+print(f'Média das Idades = {media_idade:.2f}\n')
+
+print('---' * 20)
 
 # Nome do Homem mais Velho:
-lista_idades = []
 
-for x in range(1, 5):
-    if pessoa[x]['sexo'] == 'H':
-        lista_idades.append(pessoa[x]['idade'])
-    else:
-        print('Não há homens no Grupo')
-    
+if all(pessoa['sexo'] != 'H' for pessoa in pessoas.values()):
+    print('Não tem Homem na Lista\n')
+else:
+    variavel_seguradora_nome = 'place_holder'
+    variavel_seguradora_idade = 0
+
+    for x in range(1, 5):
+        if pessoas[x]['sexo'] == 'H':
+            if pessoas[x]['idade'] > variavel_seguradora_idade:
+                variavel_seguradora_nome = pessoas[x]['nome']
+                variavel_seguradora_idade = pessoas[x]['idade']
+
+    print(f'Nome do Homem mais Velho = {variavel_seguradora_nome}\nIdade = {variavel_seguradora_idade}\n')
+
+print('---' * 20)
+
+# Quantia de Mulheres Menores que 20 Anos
+
+quantia_mulheres = 0
+
+if all(pessoa['sexo'] != 'M' for pessoa in pessoas.values()):
+    print('Não tem Mulher na Lista\n')
+else:
+    for x in range(1, 5):
+        if pessoas[x]['sexo'] == 'M' and pessoas[x]['idade'] < 20:
+            quantia_mulheres += 1
+
+    print(f'Há {quantia_mulheres} Mulheres com Menos de 20 Anos\n')
+
+sys.exit('Encerrando Programa...')
